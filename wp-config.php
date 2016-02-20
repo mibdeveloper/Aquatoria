1ec7e690de60beaeaa9d4d16556f2d0416b12fe5
Viacheslav <mibdeveloper@gmail.com>
2016-02-20 23:41:26 +0200

    clean config

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'aquatoria');
+define('DB_NAME', '');
 
 /** MySQL database username */
-define('DB_USER', 'aquatoria_db');
+define('DB_USER', '');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'DGxkZoU4aD7MaSVP');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
